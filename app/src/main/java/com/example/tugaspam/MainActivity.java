f6904bd33237ab07d335db45d46a6cd5d81841c3
PinkyRhinos <muhammadriskidhiaulhaq112@gmail.com>
2021-03-31 23:46:49 +0700

    Activity 5

--- app/src/main/java/com/example/tugaspam/MainActivity.java
+++ app/src/main/java/com/example/tugaspam/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.tugaspam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,9 +15,32 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edE,edP;
+    EditText edE, edP;
     Button btn;
+
     String nama, password;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(),DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
         btn = findViewById(R.id.btSignin);
 
 
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 nama = edE.getText().toString();
                 password = edP.getText().toString();
 
-                String email = "dama@mail.com";
+                String email = "kidul@mail.com";
                 String pass = "123";
 
                 if (nama.isEmpty() || password.isEmpty()) {
@@ -41,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
                             "Email dan Password Wajib Di Isi", Toast.LENGTH_LONG);
                     t.show();
 
-                }else{
-                    if(nama.equals(email) && password.equals(pass)) {
+                } else {
+                    if (nama.equals(email) && password.equals(pass)) {
                         Toast t = Toast.makeText(getApplicationContext(),
                                 "Login Sukses",
                                 Toast.LENGTH_LONG);
@@ -53,38 +79,16 @@ public class MainActivity extends AppCompatActivity {
                         b.putString("a", nama.trim());
                         b.putString("b", password.trim());
 
-                        Intent i = new Intent(getApplicationContext(),ActivityHasil.class);
+                        Intent i = new Intent(getApplicationContext(), Home_Activity.class);
                         i.putExtras(b);
                         startActivity(i);
-                    }else {
+                    } else {
                         Toast t = Toast.makeText(getApplicationContext(),
                                 "Login Gagal", Toast.LENGTH_LONG);
                         t.show();
                     }
-                }
+                  }
             }
-//                check();
-
-        });
+       });
     }
-
-//    public void check() {
-//        if (edE.getText().toString().equals("admin@email.com") &&
-//            edP.getText().toString().equals("yudis"))
-//        {
-//            Toast.makeText(this, "Login Sukses", Toast.LENGTH_SHORT).show();
-//        }
-//        else if (edE.getText().toString().equals("admin@email.com"))
-//        {
-//            Toast.makeText(this, "Password Salah", Toast.LENGTH_SHORT).show();
-//        }
-//        else if (edP.getText().toString().equals("yudis"))
-//        {
-//            Toast.makeText(this, "Email Salah", Toast.LENGTH_SHORT).show();
-//        }
-//        else
-//        {
-//            Toast.makeText(this, "Email dan Password Salah", Toast.LENGTH_SHORT).show();
-//        }
-//    }
 }
